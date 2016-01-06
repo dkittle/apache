d733f1bf51279c613701b92204a5e320f806b132
dkittle <don@kittle.ca>
2016-01-06 08:19:04 -0500

    forgot inventory file in script

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 # ansible-playbook -i inventory/qa.hosts playbooks/server.yml --vault-password-file ~/.vault-pass
 
-ansible-playbook server.yml --vault-password-file ~/.vault-pass
+ansible-playbook -i hosts server.yml --vault-password-file ~/.vault-pass
